073cfb1559d424bbcb74a3ea310a69fafdd3644e
PlexSir <khayak3@gmail.com>
2024-07-04 12:57:21 +0200

    project

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that provides function for adding two numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
